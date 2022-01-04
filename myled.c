8421b2c32ca8ae69db18e5b16d7a0180b42ff2ef
Kazuma Toyoshima <s20c1079ab@s.chibakoudai.jp>
2022-01-04 13:56:37 +0000

    add a myled

--- myled.c
+++ myled.c
@@ -1,13 +1,23 @@
 #include <linux/module.h>
+#include <linux/fs.h>
+
 MODULE_AUTHOR("Ryuichi Ueda");
 MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.1");
 
+static dev_t dev;
+
 static int __init init_mod(void)
 {
-	printk(KERN_INFO "%s is loaded.\n",__FILE__);
-	return 0;
+	int retval;
+	retval = alloc_chrdev_region(&dev, 0, 1, "myled");
+	if(retval < 0){
+		
+		printk(KERN_ERR "alloc_chrdev_region failed.\n");
+		return retval;
+	}	
+		return 0;
 }
 
 static void __exit cleanup_mod(void)
