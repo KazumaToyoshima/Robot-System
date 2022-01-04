4d896d1d1408037b8a12dfe124ad1507f2baa2af
Kazuma Toyoshima <s20c1079ab@s.chibakoudai.jp>
2022-01-04 12:33:02 +0000

    add a myled

--- myled.c
+++ myled.c
@@ -1,9 +1,18 @@
-
 #include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/uaccess.h>
+#include <linux/io.h>
 
 static int __init init_mod(void)
 {
 	return 0;
 }
 
-static __init init_mod
+static void __exit cleanup_mod(void)
+{
+}
+
+module_init(init_mod);
+module_exit(cleanup_mod);
