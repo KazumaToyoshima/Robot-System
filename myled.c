83f241568109c432d1cc44e98a17a682dcfe325e
KazumaToyoshima <s20c1079ab@s.chibakoudai.jp>
2022-01-07 16:21:33 +0000

    add a myled

--- myled.c
+++ myled.c
@@ -21,7 +21,7 @@ static ssize_t led_write(struct file* flip, const char* buf, size_t count, loff_
 	if(copy_from_user(&c,buf,sizeof(char)))
 	return -EFAULT;
 
-	// printk(KERN_INFO "receive %c\n" ,c);
+ printk(KERN_INFO "receive %c\n" ,c);
 	
 	if(c == '0')
 		gpio_base[10] = 1 << 25;
@@ -82,7 +82,7 @@ static int __init init_mod(void)
 		const u32 index = led/10;
 		const u32 shift = (led%10)*3;
 		const u32 mask = ~(0x7 << shift);
-		gpio_base = (gpio_base[index] & mask) | (0x1 << shift);
+		gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
 		return 0;
 }
 
